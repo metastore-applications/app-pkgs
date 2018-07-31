7f05ab81ebc844d3345291683a8ba9d536928df5
Kitsune Solar <kitsune.solar@gmail.com>
2018-08-01 00:12:22 +0300

    2018-07-31 21:12:22

--- resources/views/_common/footer.php
+++ resources/views/_common/footer.php
@@ -1,9 +1,9 @@
+<?php use MetaStore\App\Kernel\MetaCR; ?>
+
 <footer class="footer">
 	<div class="content has-text-centered">
 		<p>
-			<strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-			<a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-			is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+			<?php echo MetaCR::getCR(); ?>
 		</p>
 	</div>
 </footer>
